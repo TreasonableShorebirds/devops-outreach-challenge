a91ebb93440793fcafdb825ae58bf18640dfd873
gesparza3 <esparzagrant@gmail.com>
2019-09-09 14:35:26 -0700

    Updated url

--- react/src/components/Instructions.jsx
+++ react/src/components/Instructions.jsx
@@ -3,7 +3,8 @@ import { Segment, Form, Button } from 'semantic-ui-react';
 
 
 const host = process.env.REACT_APP_IP || 'localhost'
-const nodeUrl = 'http://'+ host + '/api'
+//const nodeUrl = 'http://'+ host + '/api'
+const nodeUrl = 'https://apprentice-outreach.flywheel-staging.lead.prod.liatr.io/api'
 
 const instructionText = [
   <div>
