1c35a202e363fd219a61eb7c874610dcb3956e31
Jordan A <jordana@Jordans-Liatrio-Macbook-Pro.local>
2019-09-10 10:35:24 -0700

    Add https

--- react/src/components/Instructions.jsx
+++ react/src/components/Instructions.jsx
@@ -3,7 +3,7 @@ import { Segment, Form, Button } from 'semantic-ui-react';
 
 
 const host = process.env.REACT_APP_IP || 'localhost'
-const nodeUrl = 'http://' + host + '/api/'
+const nodeUrl = 'https://' + host + '/api/'
 
 const instructionText = [
   <div>
