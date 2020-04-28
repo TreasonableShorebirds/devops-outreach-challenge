4bb70b93736893a8fb90ad4fe0f363390883e1ca
Empyreus <rjs@liatrio.com>
2020-04-28 13:10:05 -0700

    Inital Update of navbar

--- react/src/components/MainMenu.jsx
+++ react/src/components/MainMenu.jsx
@@ -1,91 +1,102 @@
-import React, { Component } from 'react';    
-import logo from '../logo.png';    
-import { Menu, Icon, Container, Image, Dropdown } from 'semantic-ui-react';
-    
-class MainMenu extends Component {    
-  render() {    
-    return (    
-      <Menu fixed='top' inverted>    
-        <Container>    
-          <Menu.Item    
-            header    
-            as='a'
-            href='/'
-          >    
-            <Image    
-              size='mini'    
-              src={logo}    
-              style={{ marginRight: '1.5em' }}    
-            />    
+import React, { Component } from "react";
+import logo from "../logo.png";
+import { Menu, Icon, Container, Image, Dropdown } from "semantic-ui-react";
+
+class MainMenu extends Component {
+  state = { activeItem: "home" };
+
+  render() {
+    return (
+      <Menu fixed="top" inverted>
+        <Container>
+          <Menu.Item header as="a" href="/">
+            <Image
+              size="mini"
+              src="https://assets.website-files.com/5c0ef0d637368ba8badd3577/5d38ec257a00daabc1da3514_Project-Product-icon-hex.svg"
+              style={{ marginRight: "1.5em" }}
+            />
             DevOps Challenge
-          </Menu.Item>    
-          <Dropdown item simple text='Help'>    
-            <Dropdown.Menu>    
+          </Menu.Item>
+          <Dropdown item simple text="Help">
+            <Dropdown.Menu>
               <Dropdown.Item
-                as='a'
+                as="a"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://github.com/join">
+                href="https://github.com/join"
+              >
                 Sign Up for GitHub
-              </Dropdown.Item>    
+              </Dropdown.Item>
               <Dropdown.Item
-                as='a'
+                as="a"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://help.github.com/articles/fork-a-repo/#fork-an-example-repository">
+                href="https://help.github.com/articles/fork-a-repo/#fork-an-example-repository"
+              >
                 How to Fork a Repository on GitHub
-              </Dropdown.Item>    
+              </Dropdown.Item>
               <Dropdown.Item
-                as='a'
+                as="a"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://help.github.com/articles/adding-a-file-to-a-repository/">
+                href="https://help.github.com/articles/adding-a-file-to-a-repository/"
+              >
                 How to Add a File to a Repository on GitHub
-              </Dropdown.Item>    
+              </Dropdown.Item>
               <Dropdown.Item
-                as='a'
+                as="a"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://blog.travis-ci.com/2017-08-24-trigger-custom-build">
+                href="https://blog.travis-ci.com/2017-08-24-trigger-custom-build"
+              >
                 How to Trigger a Build using the Travis CI Web Console
               </Dropdown.Item>
               <Dropdown.Item
-                as='a'
+                as="a"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://docs.travis-ci.com/user/getting-started/">
-                Getting Started with Travis CI
-              </Dropdown.Item>    
-              <Dropdown.Item
-                as='a'
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://help.github.com/articles/editing-files-in-your-repository/">
-                How to Edit a File in a Repository on GitHub
-              </Dropdown.Item>    
-              <Dropdown.Item
-                as='a'
-                onClick={ this.props.clear }
+                href="https://docs.travis-ci.com/user/getting-started/"
+                name="Getting Started with Travis CI"
               >
-                Reset Progress Tracking
+                Getting Started with Travis CI
               </Dropdown.Item>
-            </Dropdown.Menu>    
+              <Dropdown.Item
+                as="a"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://help.github.com/articles/editing-files-in-your-repository/"
+              >
+                How to Edit a File in a Repository on GitHub
+              </Dropdown.Item>
+            </Dropdown.Menu>
           </Dropdown>
-          { this.props.user ?
-            <Menu.Item    
-              header    
-              as='a'
+          {this.props.user ? (
+            <Menu.Item
+              header
+              as="a"
               target="_blank"
               rel="noopener noreferrer"
-              href={ 'https://github.com/' + this.props.user }
+              href={"https://github.com/" + this.props.user}
             >
-              <Icon name='github' />
+              <Icon name="github" />
               {this.props.user}
-            </Menu.Item> : null }
-        </Container>    
-      </Menu>    
-    );    
-  }    
-}    
-    
-export default MainMenu;  
+            </Menu.Item>
+          ) : null}
+          <Menu.Item
+            as="a"
+            href="/"
+            name="Reset Progress"
+            position="right"
+            onClick={this.props.clear}
+          />
+          <Menu.Item header as="a" href="https://www.liatrio.com/">
+            Liatrio
+            <Image size="mini" src={logo} style={{ marginLeft: "1.5em" }} />
+          </Menu.Item>
+        </Container>
+      </Menu>
+    );
+  }
+}
+
+export default MainMenu;
